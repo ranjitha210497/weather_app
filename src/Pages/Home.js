de021bdaf9d0bbb5e32254b73ae7882575e5d35f
ranjitha210497 <ranjithasampangi.s.1a2gmail.com>
2021-09-03 22:11:15 +0530

    initial

---
+++ src/Pages/Home.js
@@ -0,0 +1,34 @@
+import { div } from 'prelude-ls';
+import React, { useState } from 'react';
+import {useLazyQuery} from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/Queries';
+
+function Home() {
+    const [citySearched, setCitySearched] = useState('')
+    const [getWeather, { data, error}] = useLazyQuery(GET_WEATHER_QUERY, {
+        variables: { name: citySearched },
+    })
+    if(error) return <h1>Error found</h1>
+    if(data) {
+        console.log(data);
+    }
+    return (
+        <div className="home">
+            <h1>Search for Weather</h1>
+            <input type="text" placeholder="City name..." onChange={(e) => setCitySearched(e.target.value)}/>
+            <button onClick={() => getWeather()}>Search</button>
+            <div className="weather">
+                {data && (
+                    <>
+                    <h1>{data.getCityByName.name}</h1>
+                    <h1>Temperature: {data.getCityByName.weather.temperature.actual}</h1>
+                    <h1>Description: {data.getCityByName.weather.summary.description}</h1>
+                    <h1>Wind Speed: {data.getCityByName.weather.wind.speed}</h1>
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
